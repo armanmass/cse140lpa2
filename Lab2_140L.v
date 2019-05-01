275b701001fd7a40f74100559a35d36837f9dd79
armanmass <armanmass2002@gmail.com>
2019-05-01 03:23:21 +0000

    update

--- Lab2_140L.v
+++ Lab2_140L.v
@@ -33,22 +33,44 @@ module Lab2_140L (
 
 	reg [7:0] num1;
 	reg [7:0] num2;
-	reg [7:0] c;
-	reg [7:0] sum;
+	reg [7:0] cout;
+	wire [7:0] sum;
 	reg cin;
 
 	always @(posedge clk) begin
-		cin <= 0;
 		num1 <= Gl_r1;
 		num2 <= Gl_r2;
 		if (Gl_subtract) begin
 			num2 <= ~num2;
-			cin <= 1;
 		end
 	end
 
 
-	fb_adder fb_adder1 (.a(num1), .b(num2), .cin(cin), .cout(c), .sum(L2_adder_data), .ans(L2_led));
+	fb_adder fb_adder1 (.a(num1), .b(num2), .cin(Gl_substract), .cout(c), .sum(sum), .ans(L2_led));
+	
+	reg [7:0] char;
+	integer i;
+	
+	always @(*) begin
+	
+		for(i = 0; i < 4; i = i + 1) begin
+			char[i] = L2_led[i];
+		end
+		
+		char[7] = 1'b0;
+		char[4] = 1'b0;
+		
+		if(L2_led[4]) begin
+			char[6] = 1'b1;
+			char[5] = 1'b0;
+		end else begin
+			char[6] = 1'b0;
+			char[5] = 1'b1;
+		end
+	
+	end
+	
+	assign L2_adder_data = char;
 
 endmodule
 
@@ -82,12 +104,25 @@ module fu_adder(input a, input b, input cin,
 endmodule //fulladder
 
 
-module fb_adder(input [7:0] a, input [7:0] b, input cin, input [7:0] cout, input [7:0] sum, output [7:0] ans);
+module fb_adder(input [7:0] a, input [7:0] b, input cin, input [7:0] cout, output [7:0] sum, output reg [7:0] ans);
 
+wire cout1, cout2, cout3, cout4;
 
-fu_adder fu_adder1 (.a(a[0]), .b(b[0]), .cin(cin), .sum(sum[0]), .cout(cout[0]));
-fu_adder fu_adder2 (.a(a[1]), .b(b[1]), .cin(cout[0]), .sum(sum[1]), .cout(cout[1]));
-fu_adder fu_adder3 (.a(a[2]), .b(b[2]), .cin(cout[1]), .sum(sum[2]), .cout(cout[2]));
-fu_adder fu_adder4 (.a(a[3]), .b(b[3]), .cin(cout[2]), .sum(sum[3]), .cout(sum[4]));
+fu_adder fu_adder1 (.a(a[0]), .b(b[0]), .cin(cin), .sum(sum[0]), .cout(cout1));
+fu_adder fu_adder2 (.a(a[1]), .b(b[1]), .cin(cout1), .sum(sum[1]), .cout(cout2));
+fu_adder fu_adder3 (.a(a[2]), .b(b[2]), .cin(cout2), .sum(sum[2]), .cout(cout3));
+fu_adder fu_adder4 (.a(a[3]), .b(b[3]), .cin(cout3), .sum(sum[3]), .cout(cout4));
+
+always @(*) begin
+	ans[0] = sum[0];
+	ans[1] = sum[1];
+	ans[2] = sum[2];
+	ans[3] = sum[3];
+	ans[4] = cout4;
+	ans[5] = 1'b0;
+	ans[6] = 1'b0;
+	ans[7] = 1'b0;
+end
+
 
 endmodule //4bit adder
---
+++ simulation/modelsim/Lab2_140L_run_msim_rtl_verilog.do.bak1
@@ -0,0 +1,9 @@
+transcript on
+if {[file exists rtl_work]} {
+	vdel -lib rtl_work -all
+}
+vlib rtl_work
+vmap work rtl_work
+
+vlog -vlog01compat -work work +incdir+D:/PhotonUser/My\ Files/Home\ Folder/Lab2 {D:/PhotonUser/My Files/Home Folder/Lab2/Lab2_140L.v}
+
